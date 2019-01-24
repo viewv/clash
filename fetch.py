3e690b53488adb49bd4f11d1964abdd53dbd28ac
Zhang XueNan <zxn@zxnnet.top>
2019-01-24 18:07:38 +0800

    fix all bug

--- fetch.py
+++ fetch.py
@@ -1,7 +1,6 @@
 import os
-import sys
 import yaml
-import requests as re
+import requests
 
 URL = "https://api.dlercloud.com/link/8kyigPBojXY88d3q?is_mu=0&clash=1"
 PATH = "/home/viewv/.config/clash/"
@@ -9,28 +8,16 @@ NAME = "config.yml"
 
 print("Welcome to Fetch!")
 
-with open(PATH + NAME, "wb") as f:
-    print("Downloading Config File")
-    res = re.get(URL, stream=True)
-    total_length = res.headers.get('content-length')
+r = requests.get(URL)
 
-    if total_length is None:  # no content length header
-        f.write(res.content)
-    else:
-        dl = 0
-        total_length = int(total_length)
-        for data in res.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            done = int(50 * dl / total_length)
-            sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
-            sys.stdout.flush()
+with open(PATH + NAME, 'wb') as f:
+    f.write(r.content)
 
-with open(PATH + NAME, "w+") as f:
-    config = f.read()
-    diction = yaml.load(config)
-    diction['external-controller'] = "127.0.0.1:1234"
-    yaml.dump(dict, f)
+with open(PATH + NAME, 'r+') as f:
+    config = yaml.load(f)
+    print(config)
+    config['external-controller'] = "127.0.0.1:1234"
+    yaml.dump(config, f)
 
 os.system('pm2 restart clash-linux')
 
